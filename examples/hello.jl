64bec44f970c87f191a97dde34347d7e287fbc2f
Kristof Cools <krcools@gmail.com>
2018-11-27 17:37:07 +0100

    cleaned example

--- examples/hello.jl
+++ examples/hello.jl
@@ -1,12 +1,7 @@
 using JLMake
 
-# target_A = Target(:A, ()->rand(10))
-# target_B = Target(:B, ()->rand(10))
-# target_C = Target(:C, (A,B)->A.+B, tA, tB)
-
-
 @target A ()->rand(10)
-@target B ()->rand(9+1)
+@target B ()->rand(10)
 @target C (A,B)->A.+B
 
 
