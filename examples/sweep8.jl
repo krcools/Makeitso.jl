c88fa6dbcc8981d0864bee393c38f4328f43e594
Kristof Cools <krcools@gmail.com>
2026-07-24 13:25:07 +0200

    Removed NamedTuple() from progress output

--- examples/sweep8.jl
+++ examples/sweep8.jl
@@ -30,7 +30,7 @@ end
 # x = make(average; seed=[1.0,2.0,3.0], p=3.14)
 # y = make(solutions; seed=[1.0,2.0,3.0], p=3.14)
 
-x = make(average, verbosity(:full); seed=[1.0,2.0,3.0], p=3.14)
+x = make(average, verbosity(:variables); seed=[1.0,2.0,3.0], p=3.14)
 # y = make(solutions, verbosity(:short); seed=[1.0,2.0,3.0], p=3.14)
 
 # @info "Building zs"
